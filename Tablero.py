db097a1591e411fda576c01a21e37ddd4ee7c521
kenfabguzram <95146991+kenfabguzram@users.noreply.github.com>
2022-02-23 20:01:20 -0600

    Add methods and fields at Tablero Class
    I added numeroTurno field and jugando field to the Tablero class, also added turno y partida methods.

--- Tablero.py
+++ Tablero.py
@@ -6,7 +6,8 @@ class Tablero:
     #Defina aquí los atributos de Tablero
     espacios=0
     fichas=[]
-
+    numeroTurno=0
+    jugando=True
     #también va a necesitar una lista de Fichas (puede asumir un número de Fichas fijo si le parece más fácil), 
     #y un mecanismo para saber quién sigue en el turno
 
@@ -15,9 +16,11 @@ class Tablero:
 
     #agregue parámetros necesarios después de self
     #para iniciar los valores de sus atributos
-    def __init__(self,espacios,c_jugadores):
-        for n in range(c_jugadores):
-            self.fichas.append(Ficha(input("Jugador "+str(n+1)+", ingrese el color de ficha deseado:")))
+    def __init__(self,espacios):
+        self.fichas.append(Ficha("verde"))
+        self.fichas.append(Ficha("amarilla"))
+        self.fichas.append(Ficha("azul"))
+        self.fichas.append(Ficha("roja"))
         shuffle(self.fichas)
         self.espacios=espacios
         #inicialice aquí todos los atributos
@@ -26,3 +29,30 @@ class Tablero:
 
     #defina aquí los métodos de Tablero
     #recuerde que el primer parámetro de cada método es siempre self
+    def turno(self):
+        self.numeroTurno+=1
+        print("----------------------------------------------------------------------------------")
+        print("Turno #",self.numeroTurno)
+        for i in range(len(self.fichas)):
+            self.fichas[i].avanzar()
+            if self.fichas[i].posicion>=self.espacios:
+                self.jugando=False
+                print("La ficha ",self.fichas[i].color," es la ganadora")
+                print("----------------------------------------------------------------------------------")
+                break
+
+    def partida(self):
+        self.jugando=True
+        while self.jugando:
+            self.numeroTurno+=1
+            print("----------------------------------------------------------------------------------")
+            print("Turno #",self.numeroTurno)
+            for i in range(len(self.fichas)):
+                self.fichas[i].avanzar()
+                if self.fichas[i].posicion>=self.espacios:
+                    self.jugando=False
+                    print("La ficha ",self.fichas[i].color," es la ganadora")
+                    print("----------------------------------------------------------------------------------")
+                    break
+            if not self.jugando:
+                break
